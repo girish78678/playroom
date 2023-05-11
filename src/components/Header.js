e78de9e1391d309b16f41c01d23bf345e8399439
girish sharma <girish@sozeno.com>
2023-05-11 18:47:47 +0530

    update pricing plans

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,85 +1,60 @@
 import Link from "next/link";
-// import { openNav } from "./pages/navbar.js";
-// import { closeNav }from ".pages/navbar.js";
 import Head from "next/head.js";
 const Header = () => {
-  return (
+  return(
     <header>
-      <nav>
+        <nav>
         <div className="navbar">
           <div className="logo">
             <Link href="/">
               <img src="images/homepage/header-logo.png" alt="Logo" />
             </Link>
           </div>
+
           <div id="mySidepanel" class="sidepanel">
-            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">
-             
-           
-            </a>
-            <a class="no--border" href="#home">
-             
-              Home
-            </a>
-            <a href="#" class="nav-link">
-             
-              Pages
-            </a>
-            <a href="#" class="nav-link">
-             
-              Events
-            </a>
-            <a href="#" class="nav-link">
-             
-              Blog
-            </a>
-            <a href="#" class="nav-link">
-             
-              Shop
-            </a>
-            <a href="#" class="nav-link">
-             
-              Landing
-            </a>
-          </div>
-          <button class="openbtn"> ☰ </button>
+                    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+                    <a class="no--border" href="#home">Home</a>
+                    <a href="#" class="nav-link">Pages</a>
+                    <a href="#" class="nav-link">Events </a>
+                    <a href="#" class="nav-link"> Blog</a>
+                    <a href="#" class="nav-link"> Shop</a>
+                    <a href="#" class="nav-link"> Landing</a>
+                </div>
+                <button class="openbtn"> ☰ </button>
+                
           <div className="nav-links">
-            <ul className="no-bullets">
+            < ul className = "no-bullets">
               <li>
-                <Link href="/"> Home </Link>
+                <Link href="/">Home</Link>
               </li>
               <li>
-                <Link href="/pages"> Pages </Link>
+                <Link href="/pricing-plans">Pricing Plans</Link>
               </li>
               <li>
-                <Link href="/events"> Events </Link>
+                <Link href="/events">Events</Link>
+              </li>
+             
+              <li>
+                <Link href="/shop">Shop</Link>
               </li>
               <li>
-                <Link href="/blog"> Blog </Link>
+                <Link href="/about-us">About Us</Link>
               </li>
               <li>
-                <Link href="/shop"> Shop </Link>
+                <Link href="/grid-view">Grid Home</Link>
               </li>
               <li>
-                <Link href="/about-us"> About us</Link>
+                <Link href="/animator">Animator</Link>
               </li>
               <li>
-                <Link href="/grid-view"> Grid Home </Link>
-              </li>
-              <li>
-                <Link href="/animator"> Animator </Link>
-              </li>
-              <li>
-                <Link href="/rules--of--play"> Rules Of Play </Link>
+                <Link href="/rules--of--play">Rules Of Play</Link>
               </li>
             </ul>
           </div>
         </div>
       </nav>
-    </header>
-  );
-};
-
-export default Header;
-
-
+      </header>
+  )
+    };
+    
+    export default Header;
